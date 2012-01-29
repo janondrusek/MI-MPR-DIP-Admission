2680e7ee7f8c2778354293343d0d7af7acb1cbc2
JeanVEGA <ondrusek.jan@gmail.com>
2012-01-30 00:09:40 +0100

    Roo added to context

--- jetty.bat
+++ jetty.bat
@@ -1,5 +1,4 @@
 @ECHO OFF
-set JAVA_HOME=%JAVA_HOME_7%
 set MAVEN_OPTS=-server -Xmx512m -Xms512m -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8791,server=y,suspend=n 
 
 mvn -e clean jetty:run
