eb0b85204519ddc558be58dbce1757909c966902
Ján Ondrušek <ondrusek.jan@gmail.com>
2012-03-01 22:32:39 +0100

    automatic role and test user insert on startup, should be removed when LIVE, TODO: setup Spring profiles

---
+++ src/main/resources/META-INF/sql/admission-test-data.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO `user_identity` VALUES (1,'more');
+INSERT IGNORE INTO `user_identity_role` VALUES (1,2);
+INSERT IGNORE INTO `user_permission` VALUES (1,'PERM_READ_PERSON'),(3,'PERM_WRITE_PHOTO'),(2,'PERM_WRITE_RESULT');
+INSERT IGNORE INTO `user_role` VALUES (1,'ROLE_EXAMINER'),(2,'ROLE_SUPERVISOR');
+INSERT IGNORE INTO `user_role_permission` VALUES (1,1),(1,2),(1,3),(2,1),(2,2),(2,3);
