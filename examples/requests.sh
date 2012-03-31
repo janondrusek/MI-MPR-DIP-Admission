116f028add46a54f0d783df49bea85951abab7df
Ján Ondrušek <ondrusek.jan@gmail.com>
2012-03-31 14:36:15 +0200

    examples updated and moved

--- examples/requests.sh
+++ examples/requests.sh
@@ -7,13 +7,13 @@ curl -i -H "Accept: application/json" -H "Authorization: Basic bW9yZTptb3JlCg=="
 curl -i -H "Accept: application/json" -H "X-CTU-FIT-Admission-Session: [session identifier from Mobile.getIdentity]" http://localhost:9090/admission/services/mobile/admission/{admissionCode}
 
 # Mobile.saveResult
-cat example_admission_result.xml | curl -i -H "Accept: application/json" -H "Content-type: application/xml" -H "X-CTU-FIT-Admission-Session: [session identifier from Mobile.getIdentity]" -X POST -d @- http://localhost:9090/admission/services/mobile/admission/{admissionCode}/result
+cat admission_result.xml | curl -i -H "Accept: application/json" -H "Content-type: application/xml" -H "X-CTU-FIT-Admission-Session: [session identifier from Mobile.getIdentity]" -X POST -d @- http://localhost:9090/admission/services/mobile/admission/{admissionCode}/result
 
 # Mobile.savePhoto
-cat example_admission_photo.xml | curl -i -H "Accept: application/json" -H "Content-type: application/xml" -H "X-CTU-FIT-Admission-Session: [session identifier from Mobile.getIdentity]" -X POST -d @- http://localhost:9090/admission/services/mobile/admission/{admissionCode}/photo
+cat admission_photo.xml | curl -i -H "Accept: application/json" -H "Content-type: application/xml" -H "X-CTU-FIT-Admission-Session: [session identifier from Mobile.getIdentity]" -X POST -d @- http://localhost:9090/admission/services/mobile/admission/{admissionCode}/photo
 
 # Processing.admission, uses example admission from XML file and PUT request to add it 
-cat example_admission.xml | curl -i -H "Accept: application/json" -H "Content-type: application/xml" -X PUT -d @- http://localhost:9090/admission/services/processing/admission
+cat admission.xml | curl -i -H "Accept: application/json" -H "Content-type: application/xml" -X PUT -d @- http://localhost:9090/admission/services/processing/admission
 
 # Processing.deleteAmission, simply deletes admission in cascade with all its entites
 curl -i -X DELETE http://localhost:9090/admission/services/processing/admission/{admissionCode}
