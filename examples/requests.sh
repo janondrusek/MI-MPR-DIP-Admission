a4edda37e815ef5a195b2561f167aed87e3bdfb4
Ján Ondrušek <ondrusek.jan@gmail.com>
2012-04-19 22:56:42 +0200

    updated examples as now all resources are protected

--- examples/requests.sh
+++ examples/requests.sh
@@ -12,8 +12,11 @@ cat admission_result.xml | curl -i -H "Accept: application/json" -H "Content-typ
 # Mobile.savePhoto
 cat admission_photo.xml | curl -i -H "Accept: application/json" -H "Content-type: application/xml" -H "X-CTU-FIT-Admission-Session: [session identifier from Mobile.getIdentity]" -X POST -d @- http://localhost:9090/admission/services/mobile/admission/{admissionCode}/photo
 
+# Processing.getIdentity, username/password: more/more (DUMMY adapter)
+curl -i -H "Accept: application/json" -H "Authorization: Basic bW9yZTptb3JlCg==" http://localhost:9090/admission/services/processing/identity
+
 # Processing.admission, uses example admission from XML file and PUT request to add it 
-cat admission.xml | curl -i -H "Accept: application/json" -H "Content-type: application/xml" -X PUT -d @- http://localhost:9090/admission/services/processing/admission
+cat admission_0[1-3].xml | curl -i -H "Accept: application/json" -H "X-CTU-FIT-Admission-Session: [session identifier from Processing.getIdentity] -H "Content-type: application/xml" -X PUT -d @- http://localhost:9090/admission/services/processing/admission
 
 # Processing.deleteAmission, simply deletes admission in cascade with all its entites
-curl -i -X DELETE http://localhost:9090/admission/services/processing/admission/{admissionCode}
+curl -i -H "X-CTU-FIT-Admission-Session: [session identifier from Processing.getIdentity] -X DELETE http://localhost:9090/admission/services/processing/admission/{admissionCode}
