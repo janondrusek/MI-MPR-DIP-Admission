46211eaa6bf289869608fc7154798db69850407c
David Chobotsky <david.chobotsky@gmail.com>
2012-04-22 22:56:27 +0200

    shell script fix

--- examples/requests.sh
+++ examples/requests.sh
@@ -16,7 +16,7 @@ cat admission_photo.xml | curl -i -H "Accept: application/json" -H "Content-type
 curl -i -H "Accept: application/json" -H "Authorization: Basic bW9yZTptb3JlCg==" http://localhost:9090/admission/services/processing/identity
 
 # Processing.admission, uses example admission from XML file and POST request to add it 
-cat admission_0[1-3].xml | curl -i -H "Accept: application/json" -H "X-CTU-FIT-Admission-Session: [session identifier from Processing.getIdentity] -H "Content-type: application/xml" -X POST -d @- http://localhost:9090/admission/services/processing/admission
+cat admission_0[1-3].xml | curl -i -H "Accept: application/json" -H "X-CTU-FIT-Admission-Session: [session identifier from Processing.getIdentity]" -H "Content-type: application/xml" -X POST -d @- http://localhost:9090/admission/services/processing/admission
 
 # Processing.deleteAmission, simply deletes admission in cascade with all its entites
 curl -i -H "X-CTU-FIT-Admission-Session: [session identifier from Processing.getIdentity] -X DELETE http://localhost:9090/admission/services/processing/admission/{admissionCode}
