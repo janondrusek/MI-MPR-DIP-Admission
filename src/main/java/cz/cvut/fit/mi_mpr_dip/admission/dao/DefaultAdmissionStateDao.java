15c3bc6748c429270f4c6d8324082147d11c6963
David Chobotsky <david.chobotsky@gmail.com>
2012-04-24 14:21:57 +0200

    AdmissionStateDao

---
+++ src/main/java/cz/cvut/fit/mi_mpr_dip/admission/dao/DefaultAdmissionStateDao.java
@@ -0,0 +1,32 @@
+package cz.cvut.fit.mi_mpr_dip.admission.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import cz.cvut.fit.mi_mpr_dip.admission.domain.AdmissionState;
+
+@Repository
+public class DefaultAdmissionStateDao extends AbstractDao<AdmissionState> implements AdmissionStateDao {
+
+	@Transactional(readOnly = true)
+	@Override
+	public AdmissionState getAdmissionState(String code) {
+		return getAdmissionStateQuietly(code);
+	}
+	
+	private AdmissionState getAdmissionStateQuietly(String code) {
+		AdmissionState admissionState;
+		try {
+			admissionState = uniqueResult(AdmissionState.findAdmissionStatesByCodeEquals(code));
+		} catch (Exception e) {
+			admissionState = processException(e);
+		}
+		return admissionState;
+	}
+
+	@Override
+	protected AdmissionState createEmptyResult() {
+		return new AdmissionState();
+	}
+
+}
