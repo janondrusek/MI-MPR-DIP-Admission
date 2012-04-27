89c86b3d93c9f09ca90e63e281ada7af4228ae30
Ján Ondrušek <ondrusek.jan@gmail.com>
2012-04-27 19:23:28 +0200

    LDAP as default auth type

--- src/main/resources/META-INF/sql/admission-test-data.sql
+++ src/main/resources/META-INF/sql/admission-test-data.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO `user_identity` (`username`, `authentication`) VALUES ('more', 'PWD');
+INSERT IGNORE INTO `user_identity` (`username`, `authentication`) VALUES ('more', 'LDAP');
 INSERT IGNORE INTO `user_identity_role` 
   SELECT `user_identity_id`, `user_role_id`
   FROM `user_identity`, `user_role` WHERE `username` = 'more' AND `name` = 'ROLE_OMNI_ADMIN';
